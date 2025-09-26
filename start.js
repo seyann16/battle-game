786672ad2459f853eccedd9828a4b15f192a1268
seyann16 <mabbicgwapo@gmail.com>
2025-09-26 17:59:56 +0800

    Modified the whole implementation with exports and integration with enhanced battle game

--- start.js
+++ start.js
@@ -1,22 +1,40 @@
-const Warrior = require("./Warrior");
-const Mage = require("./Mage");
-const Archer = require("./Archer");
-const BattleGame = require("./BattleGame");
+const EnhancedBattleGame = require("./EnhancedBattleGame");
+const Warrior = require("./characters/Warrior");
+const Mage = require("./characters/Mage");
+const Archer = require("./characters/Archer");
+const Berserker = require("./characters/Berserker");
+const Archmage = require("./characters/Archmage");
+const { createNinja } = require("./factory/Ninja");
+const PetMixin = require("./abilities/pet");
 
-// Create our battle game
-const game = new BattleGame();
+// Create an epic battle with all character types!
+function createEpicBattle() {
+    const game = new EnhancedBattleGame();
 
-// Create characters of different types
-const warrior = new Warrior("Conan", 100, 15);
-const mage = new Mage("Gandalf", 80, 10);
-const archer = new Archer("Legolas", 90, 12);
-const warrior2 = new Warrior("Hercules", 110, 14);
+    console.log("\n=== EPIC CHARACTER BATTLE ===");
+    console.log("Featuring multi-level inheritance, mixins, and factory functions!\n");
 
-// Add characters to the game
-game.addCharacter(warrior);
-game.addCharacter(mage);
-game.addCharacter(archer);
-game.addCharacter(warrior2);
+    // Base characters
+    game.addCharacter(new Warrior("Conan", 100, 15));
+    game.addCharacter(new Mage("Gandalf", 80, 10));
+    game.addCharacter(new Archer("Legolas", 90, 12));
 
-// Start the battle
-game.startBattle()
+    // Specialized characters
+    game.addCharacter(new Berserker("Grom", 20, 18));
+    game.addCharacter(new Archmage("Elminster", 70, 15));
+
+    // Factory-created characters
+    game.addCharacter(createNinja("Hanzo", 85, 14));
+
+    // Characters with mixins
+    const beastMaster = new Mage("Merill", 75, 8);
+    beastMaster.summonPet("Wolf");
+    game.addCharacter(beastMaster);
+
+    game.startBattle();
+
+    return game;
+}
+
+// Run the epic battle
+const epicBattle = createEpicBattle();
